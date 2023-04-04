f03cde1b27f5f2149f2964f0b17e4f36d1006d3d
keshavsoft <nknnkumar@yahoo.com>
2023-04-04 15:46:28 +0530

    keshav

--- public/pages/Purchases/Vouchers/ShowQrCodes/Pages/Pricing/KeyPressFuncs.js
+++ public/pages/Purchases/Vouchers/ShowQrCodes/Pages/Pricing/KeyPressFuncs.js
@@ -1,14 +1,15 @@
 let StartFunc = () => {
     let jVarLocalCostValue = document.getElementById("ValueAdditionId");
+    let jVarLocalMRPId = document.getElementById("MRPId");
 
     let jVarLocalRate = document.getElementById("RateId1");
-    let jVarLocalQty = document.getElementById("QtyId1");
+    let jVarLocalValueAdditionId = document.getElementById("ValueAdditionId");
 
     jVarLocalCostValue.addEventListener("keypress", (event) => {
         if (event.keyCode === 13) {
-            let localCal = jVarLocalRate.value * jVarLocalQty.value;
-            jVarLocalCostValue.value = parseInt(localCal);
-
+            event.preventDefault();
+            let localCal = jVarLocalRate.value * (1 + (jVarLocalValueAdditionId.value / 100));
+            jVarLocalMRPId.value = parseInt(localCal);
         }
     });
 };
