2401cde3a5c86f4c51a05bd50a1efd85c04c873e
JatinNeeraj <jatinucm123@gmail.com>
2023-04-05 10:58:55 +0530

    neeraj

--- public/pages/Purchases/Vouchers/ShowQrCodes/Pages/Pricing/KeyPressFuncs.js
+++ public/pages/Purchases/Vouchers/ShowQrCodes/Pages/Pricing/KeyPressFuncs.js
@@ -1,15 +1,17 @@
 let StartFunc = () => {
-    let jVarLocalCostValue = document.getElementById("ValueAdditionId");
     let jVarLocalMRPId = document.getElementById("MRPId");
-
+    let jVarLocalQtyId1 = document.getElementById("QtyId1")
+    let jvarLocalAmountId = document.getElementById("AmountId")
     let jVarLocalRate = document.getElementById("RateId1");
     let jVarLocalValueAdditionId = document.getElementById("ValueAdditionId");
 
-    jVarLocalCostValue.addEventListener("keypress", (event) => {
+    jVarLocalValueAdditionId.addEventListener("keypress", (event) => {
         if (event.keyCode === 13) {
             event.preventDefault();
             let localCal = jVarLocalRate.value * (1 + (jVarLocalValueAdditionId.value / 100));
             jVarLocalMRPId.value = parseInt(localCal);
+            let jVarLocalCostValue = jVarLocalQtyId1.value * jVarLocalRate.value
+            console.log("jVarLocalCostValue",jVarLocalCostValue);
         }
     });
 };
