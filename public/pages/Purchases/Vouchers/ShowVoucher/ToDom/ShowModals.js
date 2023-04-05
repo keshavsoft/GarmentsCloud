c5f76de017144fe1bb92a81ca34010a8dc299345
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-04-05 17:50:05 +0530

    SREENIVAS

--- public/pages/Purchases/Vouchers/ShowVoucher/ToDom/ShowModals.js
+++ public/pages/Purchases/Vouchers/ShowVoucher/ToDom/ShowModals.js
@@ -35,15 +35,21 @@ let jVarLocalModalForQrCodeOnlyFuncs = () => {
             // Button that triggered the modal
             const button = event.relatedTarget
             // Extract info from data-bs-* attributes
+            const recipientKSno = button.getAttribute('data-ksno')
             const recipient = button.getAttribute('data-rowpk')
             const jVarLocalproductname = button.getAttribute("data-productname");
             const jVarLocalSalePrice = button.getAttribute("data-saleprice");
             const jVarLocalUserdescription = button.getAttribute("data-Userdescription");
+            const jVarLocalpurchasepk = button.getAttribute("data-purchasepk");
+            const jVarLocalinventoryserial = button.getAttribute("data-inventoryserial");
+            const jVarLocalAliesName= document.getElementById("AliesNameId").innerHTML;
+
 
             // Update the modal's content.
             const modalTitle = exampleModal.querySelector('.modal-title')
             const modalBodyInput = exampleModal.querySelector('.modal-body input')
             let jVarLocalmodalfooter = exampleModal.querySelector(".modal-footer button.ModalButtonForImageClass");
+            let jVarOnModalQrCodeOnModalId = exampleModal.querySelector('#QrCodeOnModalId');
             let jVarOnModalProductNameModalId = exampleModal.querySelector('#ProductNameModalId');
             let jVarOnModalSalePriceModalModalId = exampleModal.querySelector('#SalePriceModalId');
             let jVarOnModalUserDescriptionModalId = exampleModal.querySelector('#UserDescriptionModalId');
@@ -51,14 +57,15 @@ let jVarLocalModalForQrCodeOnlyFuncs = () => {
             modalTitle.textContent = `QrCode : ${recipient}`;
 
             GenerateQrCodeOnModal({
-                inQrData: `M-${recipient}/${jVarLocalproductname}/${jVarLocalUserdescription}/${jVarLocalSalePrice}`,
+                inQrData: `M-${recipient}/${jVarLocalproductname}/${jVarLocalAliesName}-${jVarLocalpurchasepk}-${jVarLocalinventoryserial}/${jVarLocalSalePrice}`,
                 inCanvasId: document.getElementById("CanvasId")
             });
-
-
+            
+            
+            jVarOnModalQrCodeOnModalId.innerHTML = `M-${recipient}`;
             jVarOnModalProductNameModalId.innerHTML = jVarLocalproductname;
             jVarOnModalSalePriceModalModalId.innerHTML = jVarLocalSalePrice;
-            jVarOnModalUserDescriptionModalId.innerHTML = jVarLocalUserdescription;
+            jVarOnModalUserDescriptionModalId.innerHTML = `${jVarLocalAliesName}-${jVarLocalpurchasepk}-${jVarLocalinventoryserial}`;
 
             //  jVarLocalmodalfooter.dataset.rowpk = recipient;
         })
