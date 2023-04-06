a9940cc1e442416a5c9affbaa3043641695b1fdf
keshavsoft <nknnkumar@yahoo.com>
2023-04-06 22:52:54 +0530

    keshav

---
+++ public/pages/Sales/BillPrint/Show/Show.js
@@ -0,0 +1,21 @@
+import { jFStartFunc as CheckUserFuncsjFStartFunc } from "../../../../CommonFuncs/LoginFuncs/CheckUserFuncs.js";
+import { StartFunc as ShowOnDomStartFunc } from "../Show/ToDom/ShowOnDom.js";
+import { ConfigObject } from "../../../ApiConfig.js";
+import { KeysObject } from "../ConfigKeys.js";
+
+let jVarCommonKToken = ConfigObject.TokenName;
+let jVarLocalStorageKeyName = ConfigObject.LocalStorageKeyName;
+let jVarCommonKeys = KeysObject.CommonKeys;
+let jVarCommonProjectName = ConfigObject.ProjectName;
+
+let jFStartFunc = async () => {
+    CheckUserFuncsjFStartFunc({
+        inUserKey: jVarLocalStorageKeyName,
+        inKTokenKey: jVarCommonKToken
+    });
+
+    await ShowOnDomStartFunc({ ...jVarCommonKeys, inProjectName: jVarCommonProjectName });
+};
+
+jFStartFunc().then(() => {
+});
---
+++ public/pages/Sales/BillPrint/Show/ToDom/InvGrid.js
@@ -0,0 +1,31 @@
+import { StartFunc as TableRowStartFunc } from "../FetchFuncs/HtmlPull/TableRow.js";
+
+let StartFunc = async ({ inData }) => {
+    await ShowOnDomTableBody({ inData });
+};
+
+let ShowOnDomTableBody = async ({ inData }) => {
+    let jVarLocalTableBodyId = document.getElementById("ItemsTableBodyId");
+    let jVarLocalTemplate = await TableRowStartFunc();
+
+    if (jVarLocalTemplate.KTF) {
+        jVarLocalTableBodyId.innerHTML = "";
+        var template = Handlebars.compile(jVarLocalTemplate.HtmlString);
+
+        Object.entries(inData).forEach(
+            ([key, value]) => {
+                value.pk = key;
+                value.FK = inData.pk;
+                value.SupplierName = inData.SupplierName;
+                value.BillNumber = inData.BillNumber;
+                value.Date = inData.Date;
+
+                let jVarLocalToShowHtml = template(value);
+
+                jVarLocalTableBodyId.insertAdjacentHTML("afterbegin", jVarLocalToShowHtml);
+            }
+        );
+    };
+};
+
+export { StartFunc };
