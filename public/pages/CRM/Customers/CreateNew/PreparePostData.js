7110964e03797700148d98e14d2943557ab87a73
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-04-07 18:06:13 +0530

    ram

--- public/pages/CRM/Customers/CreateNew/PreparePostData.js
+++ public/pages/CRM/Customers/CreateNew/PreparePostData.js
@@ -1,10 +1,20 @@
 let StartFunc = () => {
     let jVarLocalCustomerName = document.getElementById("CustomerName");
-    let jVarLocalGSTId = document.getElementById("GSTId");
+    let jVarLocalCustomerAddress = document.getElementById("CustomerAddress");
+    let jVarLocalCustomerMobileNo = document.getElementById("CustomerMobileNo");
+    let jVarLocalCustomerDOB = document.getElementById("CustomerDOB");
+    let jVarLocalCustomerGender = document.getElementById("CustomerGender");
+    let jVarLocalCustomerEmail = document.getElementById("CustomerEmail");
+    
 
     let jVarLocalReturnData = {};
     jVarLocalReturnData.CustomerName = jVarLocalCustomerName.value;
-    jVarLocalReturnData.GST = jVarLocalGSTId.value;
+    jVarLocalReturnData.CustomerAddress = jVarLocalCustomerAddress.value;
+    jVarLocalReturnData.CustomerMobileNo = jVarLocalCustomerMobileNo.value;
+    jVarLocalReturnData.CustomerDOB = jVarLocalCustomerDOB.value;
+    jVarLocalReturnData.CustomerGender = jVarLocalCustomerGender.value;
+    jVarLocalReturnData.CustomerEmail = jVarLocalCustomerEmail.value;
+ 
 
 
     console.log("jVarLocalReturnData : ", jVarLocalReturnData);
