61dc38332050085b233db9ce653e88b2a1b49f55
Keshav <nknnkumar@yahoo.com>
2023-04-10 13:34:03 +0530

    keshav

--- public/pages/Sales/Bills/Show/ToDom/InvGrid.js
+++ public/pages/Sales/Bills/Show/ToDom/InvGrid.js
@@ -1,30 +1,39 @@
 import { StartFunc as TableRowStartFunc } from "../FetchFuncs/HtmlPull/TableRow.js";
 
-let StartFunc = async ({ inData }) => {
-    await ShowOnDomTableBody({ inData });
+let StartFunc = async ({ inDataAsArray }) => {
+    await ShowOnDomTableBody({ inDataAsArray });
 };
 
-let ShowOnDomTableBody = async ({ inData }) => {
+let ShowOnDomTableBody = async ({ inDataAsArray }) => {
     let jVarLocalTableBodyId = document.getElementById("InvTableBodyId");
     let jVarLocalTemplate = await TableRowStartFunc();
 
     if (jVarLocalTemplate.KTF) {
         jVarLocalTableBodyId.innerHTML = "";
         var template = Handlebars.compile(jVarLocalTemplate.HtmlString);
+        console.log("inDataAsArray ", inDataAsArray);
 
-        Object.entries(inData).forEach(
-            ([key, value]) => {
-                value.pk = key;
-                value.FK = inData.pk;
-                value.SupplierName = inData.SupplierName;
-                value.BillNumber = inData.BillNumber;
-                value.Date = inData.Date;
+        inDataAsArray.forEach(element => {
+            let jVarLocalToShowHtml = template(element);
 
-                let jVarLocalToShowHtml = template(value);
+            jVarLocalTableBodyId.insertAdjacentHTML("afterbegin", jVarLocalToShowHtml);
+        });
 
-                jVarLocalTableBodyId.insertAdjacentHTML("afterbegin", jVarLocalToShowHtml);
-            }
-        );
+        // Object.entries(inData).forEach(
+        //     ([key, value]) => {
+        //         value.pk = key;
+        //         value.FK = inData.pk;
+        //         value.SupplierName = inData.SupplierName;
+        //         value.BillNumber = inData.BillNumber;
+        //         value.Date = inData.Date;
+
+        //         let jVarLocalToShowHtml = template(value);
+
+        //         jVarLocalTableBodyId.insertAdjacentHTML("afterbegin", jVarLocalToShowHtml);
+        //     }
+        // );
+
+
     };
 };
 
