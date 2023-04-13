cf62bc80526c3b01eb86afe4d431f8b4cb83aa32
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-04-13 11:53:19 +0530

    keshav

--- public/assets/js/Maguvaa/pages/authentication/signin/illustration/AddListeners.js
+++ public/assets/js/Maguvaa/pages/authentication/signin/illustration/AddListeners.js
@@ -1,7 +1,8 @@
 
 let jFStartFunc = async ({ inKeyName }) => {
     let jVarSignInButtonId = document.getElementById("SignInButtonId");
-    jVarSignInButtonId.addEventListener("click", () => {
+    jVarSignInButtonId.addEventListener("click", (event) => {
+        event.preventDefault();
         jFButtonClick({ inKeyName });
     });
 };
@@ -44,27 +45,6 @@ let jFGetTokenFromFetch = async ({ inUserName, inPassWord }) => {
     let jVarLocalResponse = await fetch(jVarLocalFetchUrl, jVArLocalHeader);
     let jVarLocalResult = await jVarLocalResponse.json();
     return await jVarLocalResult;
-};
-
-let setCookie = (name, value, days) => {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-};
-
-let getCookie = (name) => {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
 };
 
 export { jFStartFunc }
