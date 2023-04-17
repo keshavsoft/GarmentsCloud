d334a63dd7821e9c1e201455e63f69351471dca3
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-04-17 20:30:55 +0530

    SREENIVAS

--- public/pages/QrCodes/DisCounts/CreateNew/PreparePostData.js
+++ public/pages/QrCodes/DisCounts/CreateNew/PreparePostData.js
@@ -1,90 +1,41 @@
 let StartFunc = () => {
-    let jVarLocalProductNameId = document.getElementById("ItemNameId");
-    let jVarLocalProductAliasNameId = document.getElementById("ProductAliasNameId");
-    // let jVarLocalBarcodeId = document.getElementById("BarcodeId");
-    let jVarLocalPrice1Id = document.getElementById("Price1Id");
-    let jVarLocalPrice2Id = document.getElementById("Price2Id");
-    let jVarLocalPecentagePrice = document.getElementById("PecentagePrice");
+    let jVarLocalQrCodeID = document.getElementById("QrCodeID");
+    let jVarLocalDateTimeEnteredID = document.getElementById("DateTimeEnteredID");
+    let jVarLocalUserNameEnteredID = document.getElementById("UserNameEnteredID");
+    let jVarLocalDiscountID = document.getElementById("DiscountID");
     let jVarLocaleditDeschiption = document.getElementById("edit-deschiption").querySelector("p");
-    let jVarLocalInventorySerial = document.getElementById("InventorySerialId");
-    let jVarLocalPurchasePk = document.getElementById("PurchasePkId");
+    let jVarLocalDiscountRsID = document.getElementById("DiscountRsID");
 
 
     let jVarLocalReturnData = {};
 
-    if (jVarLocalProductNameId !== null) {
-        jVarLocalReturnData.ProductName = jVarLocalProductNameId.value;
+    if (jVarLocalQrCodeID !== null) {
+        jVarLocalReturnData.QrCode = jVarLocalQrCodeID.value;
     };
 
-    if (jVarLocalProductAliasNameId !== null) {
-        jVarLocalReturnData.ProductAliasName = jVarLocalProductAliasNameId.value;
+    if (jVarLocalDateTimeEnteredID !== null) {
+        jVarLocalReturnData.DateTimeEntered = jVarLocalDateTimeEnteredID.value;
     };
 
-    // if (jVarLocalBarcodeId !== null) {
-    //     jVarLocalReturnData.Barcode = jVarLocalBarcodeId.value;
-    // };
-
-    if (jVarLocalPrice1Id !== null) {
-        jVarLocalReturnData.CostPrice = parseInt(jVarLocalPrice1Id.value);
+    if (jVarLocalUserNameEnteredID !== null) {
+        jVarLocalReturnData.UserNameEntered = jVarLocalUserNameEnteredID.value;
     };
 
-    if (jVarLocalPrice2Id !== null) {
-        jVarLocalReturnData.SalePrice = parseInt(jVarLocalPrice2Id.value);
+    if (jVarLocalDiscountID !== null) {
+        jVarLocalReturnData.Discount = parseInt(jVarLocalDiscountID.value);
     };
-    if (jVarLocalPecentagePrice !== null) {
-        jVarLocalReturnData.PercentageValueAddition = parseInt(jVarLocalPecentagePrice.value);
-    };
+
 
     if (jVarLocaleditDeschiption !== null) {
         jVarLocalReturnData.UserDescription = jVarLocaleditDeschiption.innerHTML;
     };
 
-    if (jVarLocalInventorySerial !== null) {
-        jVarLocalReturnData.InventorySerial = jVarLocalInventorySerial.value;
+    if (jVarLocalDiscountRsID !== null) {
+        jVarLocalReturnData.DiscountRs = parseInt(jVarLocalDiscountRsID.value);
     };
-
-    if (jVarLocalPurchasePk !== null) {
-        jVarLocalReturnData.PurchasePk = jVarLocalPurchasePk.value;
-    };
-
-    jVarLocalSupplierNameFunc({ inData: jVarLocalReturnData });
-    jVarLocalBillNumber({ inData: jVarLocalReturnData });
-    jVarLocalCommonPrepareObject({
-        inData: jVarLocalReturnData,
-        inIdName: "DateId"
-    });
 
     return jVarLocalReturnData;
 };
 
-let jVarLocalSupplierNameFunc = ({ inData }) => {
-    let jVarLocalSupplierNameSelectId = "SupplierNameSelectId"
-    let jVarLocalHtmlSupplierNameSelectId = document.getElementById(jVarLocalSupplierNameSelectId);
-
-    if ((jVarLocalHtmlSupplierNameSelectId === null) === false) {
-        let jVarLocalSupplierName = jVarLocalHtmlSupplierNameSelectId.name;
-        inData[jVarLocalSupplierName] = jVarLocalHtmlSupplierNameSelectId.value;
-    };
-};
-
-let jVarLocalBillNumber = ({ inData }) => {
-    let jVarLocalSupplierNameSelectId = "BillNumberId"
-    let jVarLocalHtmlSupplierNameSelectId = document.getElementById(jVarLocalSupplierNameSelectId);
-
-    if ((jVarLocalHtmlSupplierNameSelectId === null) === false) {
-        let jVarLocalSupplierName = jVarLocalHtmlSupplierNameSelectId.name;
-        inData[jVarLocalSupplierName] = jVarLocalHtmlSupplierNameSelectId.value;
-    };
-};
-
-let jVarLocalCommonPrepareObject = ({ inData, inIdName }) => {
-    let jVarLocalSupplierNameSelectId = inIdName;
-    let jVarLocalHtmlSupplierNameSelectId = document.getElementById(jVarLocalSupplierNameSelectId);
-
-    if ((jVarLocalHtmlSupplierNameSelectId === null) === false) {
-        let jVarLocalSupplierName = jVarLocalHtmlSupplierNameSelectId.name;
-        inData[jVarLocalSupplierName] = jVarLocalHtmlSupplierNameSelectId.value;
-    };
-};
 
 export { StartFunc };
