66dfddde39cce522afc95734897ea07aa84706ca
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-04-19 15:02:32 +0530

    SREENIVAS

--- public/pages/Sales/Bills/Show/ToDom/ShowOnDom.js
+++ public/pages/Sales/Bills/Show/ToDom/ShowOnDom.js
@@ -34,9 +34,11 @@ let localInventeryShow = async ({ inFolderName, inFileName, inItemName, inProjec
     });
 
     if (jVarLocalDataToShow.KTF) {
-        let localdata = jVarLocalDataToShow.JsonData
+        let localdata = jVarLocalDataToShow.JsonData;
 
         await InvGridStartFunc({ inDataAsArray: localdata });
+        jVarLocalShowInventorySerial({ inData: localdata });
+
     };
 };
 
@@ -57,21 +59,12 @@ let ShowOnDom = ({ inData, inShowSuccess }) => {
     };
 
     ShowSuccessFunc({ inShowSuccess });
-    jVarLocalShowInventorySerial({ inData });
 };
 
 let jVarLocalShowInventorySerial = ({ inData }) => {
     let jVarLocalSnoid = document.getElementById("Snoid");
+    jVarLocalSnoid.value = Object.keys(inData).length + 1;
 
-    if ("InvGrid" in inData) {
-        jVarLocalSnoid.value = Object.keys(inData.InvGrid).length + 1;
-
-        if (Object.keys(inData.InvGrid).length === 0) {
-            // jVarLocalSnoid.value = 1;
-        } else {
-
-        };
-    };
 };
 
 let ShowSuccessFunc = ({ inShowSuccess }) => {
