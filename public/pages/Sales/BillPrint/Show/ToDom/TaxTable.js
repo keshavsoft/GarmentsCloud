83c2aca5cc38350f22a0f47e48cc415a36738510
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-04-19 18:51:27 +0530

    keshav

--- public/pages/Sales/BillPrint/Show/ToDom/TaxTable.js
+++ public/pages/Sales/BillPrint/Show/ToDom/TaxTable.js
@@ -7,9 +7,52 @@ let StartFunc = async () => {
     let inData = localStorage.getItem('InventoryData');
     let jVarLocalGroupedData = LocalGroupDataFunc(JSON.parse(inData));
     console.log("jVarLocalGroupedData : ", jVarLocalGroupedData);
-
+    ShowOnDomTableBody({ inData: jVarLocalGroupedData });
     // LocalTotalFunc(inData);
+    jFLocalShowTaxTotals({ inData: jVarLocalGroupedData });
 };
+
+let jFLocalShowTaxTotals = ({ inData }) => {
+    jFLocalShowTotalTaxAmount({ inData });
+    jFLocalShowTotalAmount({ inData });
+};
+
+let jFLocalShowTaxTotals1 = ({ inData }) => {
+    let jVarLocalTotalTaxAmountId = document.getElementById("TotalTaxAmountId");
+
+    let jVarLocalGstAmountArray = inData.map(element => {
+        return parseFloat(element.GstAmount);
+    });
+
+    const jVarLocalAmountSum = jVarLocalGstAmountArray.reduce((a, b) => a + b, 0);
+
+    jVarLocalTotalTaxAmountId.innerHTML = jVarLocalAmountSum;
+};
+
+let jFLocalShowTotalTaxAmount = ({ inData }) => {
+    let jVarLocalTotalTaxAmountId = document.getElementById("TotalTaxAmountId");
+
+    let jVarLocalGstAmountArray = inData.map(element => {
+        return parseFloat(element.GstAmount);
+    });
+
+    const jVarLocalAmountSum = jVarLocalGstAmountArray.reduce((a, b) => a + b, 0);
+
+    jVarLocalTotalTaxAmountId.innerHTML = jVarLocalAmountSum;
+};
+
+let jFLocalShowTotalAmount = ({ inData }) => {
+    let jVarLocalTotalAmountId = document.getElementById("TotalGSTAmountId");
+
+    let jVarLocalGstAmountArray = inData.map(element => {
+        return parseFloat(element.Amount);
+    });
+    
+    const jVarLocalAmountSum = jVarLocalGstAmountArray.reduce((a, b) => a + b, 0);
+
+    jVarLocalTotalAmountId.innerHTML = jVarLocalAmountSum;
+};
+
 
 let ShowOnDomTableBody = async ({ inData }) => {
     let jVarLocalTableBodyId = document.getElementById("GstTableBodyId");
@@ -63,7 +106,7 @@ let LocalGroupDataFunc = (inData) => {
         element.GstAmount = (element.Amount * (element.GST / (100 + element.GST))).toFixed(2);
         return element;
     });
-    ShowOnDomTableBody({ inData: jVarLocalWithTaxAmountArray });
+
     return jVarLocalWithTaxAmountArray;
 };
 
