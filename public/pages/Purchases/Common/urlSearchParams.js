266dc89b8f0edb46f587eb3218344abad6298890
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-04-21 19:13:33 +0530

    SREENIVAS

---
+++ public/pages/Purchases/Common/urlSearchParams.js
@@ -0,0 +1,10 @@
+let ReturnRowPK = () => {
+    let jVarLocalSearch = document.location.search;
+    const params = new URLSearchParams(jVarLocalSearch);
+    const RowPK = params.get("RowPK");
+
+    return { RowPK };
+};
+
+
+export { ReturnRowPK }
