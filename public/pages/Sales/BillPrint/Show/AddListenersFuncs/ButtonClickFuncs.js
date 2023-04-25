25798d4f4f7f1c1d9b6aabb6969ebdccb46a506c
Keshav <nknnkumar@yahoo.com>
2023-04-25 18:21:30 +0530

    keshav

--- public/pages/Sales/BillPrint/Show/AddListenersFuncs/ButtonClickFuncs.js
+++ public/pages/Sales/BillPrint/Show/AddListenersFuncs/ButtonClickFuncs.js
@@ -1,12 +1,12 @@
 let StartFunc = () => {
     let LocalPrintId = document.getElementById("PrintId");
     LocalPrintId.addEventListener("click", jFLocalClickFunc)
-
 };
+
 const jFLocalPrintHeader = () => {
     let jVarLocalBillData = localStorage.getItem("BillData");
     let jVarLocalBillDataAsJson = JSON.parse(jVarLocalBillData);
-    console.log("jVarLocalBillDataAsJson : ", jVarLocalBillDataAsJson);
+
     let k1 = document.getElementById("PrintDiv");
     let k2 = document.getElementById("TemplateForFirmHeading");
     let k3 = document.getElementById("TemplateForTerms");
@@ -26,18 +26,32 @@ const jFLocalPrintGrid = () => {
     let localTotalDiscountId = document.getElementById("TotalDiscountId")
     let localTotalNettAmountId = document.getElementById("TotalNettAmountId")
     // console.log("jVarLocalInventoryDataAsJson : ", jVarLocalInventoryDataAsJson);
-    
+
     let k1 = document.getElementById("PrintDiv");
     let k2 = document.getElementById("TemplateForGridHeader");
     let k3 = document.getElementById("TemplateForGrossTotal");
 
     k1.innerHTML += k2.innerHTML;
-    k1.innerHTML +=
-        `${jVarLocalInventoryDataAsJson.map(function (item) {
-            return `
-        ${item.sno}      M-${item.pk}/${item.ItemName}            ${item.UnitRate}<br>
-        ${item.GST}%           ${item.DisPercentage}%                   ${item.GrossAmout}<br>`
-        })}   --------------------------------------------<br>`
+
+    // k1.innerHTML +=
+    //     `${jVarLocalInventoryDataAsJson.map(function (item) {
+    //         return `
+    //     ${item.sno}      M-${item.pk}/${item.ItemName}            ${item.UnitRate}<br>
+    //     ${item.GST}%           ${item.DisPercentage}%                   ${item.GrossAmout}<br>`
+    //     })}   --------------------------------------------<br>`
+
+    jVarLocalInventoryDataAsJson.forEach(element => {
+        let jVarLoopInsideItem = `M-${element.pk}/${element.ItemName}`;
+        let jVarLoopInsideRate = `${element.UnitRate}`;
+
+        // k1.innerHTML += `${element.sno.toString().padStart(3, "")}${" ".repeat(3)}${jVarLoopInsideItem.padEnd(25)}${jVarLoopInsideRate.padStart(15)}\n`;
+
+        k1.innerHTML += `${element.sno.toString().padStart(3, "")}`;
+        k1.innerHTML += `${" ".repeat(3)}${jVarLoopInsideItem.padEnd(25)}`;
+        k1.innerHTML += `${jVarLoopInsideRate.padStart(15)}\n`;
+    });
+
+
     k1.innerHTML += `                             Gross Amount:  ${localTotalAmountId.innerHTML}<br>
                             Total Discount:  ${localTotalDiscountId.innerHTML}<br>
                                 Net Amt:    ${localTotalNettAmountId.innerHTML}
